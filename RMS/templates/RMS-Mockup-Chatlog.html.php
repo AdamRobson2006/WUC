7ffc8583fe97a08a783dbc87230d2be11d07442b
Adam Robson <adamrobson06@outlook.com>
2026-05-12 10:11:27 +0100

    Chatpage modified to fit controller layout & connected with DB

---
+++ RMS/templates/RMS-Mockup-Chatlog.html.php
@@ -0,0 +1,32 @@
+<?php
+require "../templates/RMS-Mockup-HEADER.html";
+?>
+
+
+<h1>Staff Chat</h1>
+
+<div id="chatWrapper">
+
+    <div id="chatFeed">
+        <?= $messagesOutput ?>
+    </div>
+
+    <div id="chatInputBar">
+
+        <form action="/index.php/send" method="POST">
+
+            <input type="text" name="message" placeholder="Type a message..." required>
+
+            <input type="hidden" name="tempSessionID" value="<?= $tempSessionID?>">
+
+            <button type="submit">Send</button>
+
+        </form>
+
+    </div>
+
+</div>
+
+<?php
+require "../templates/RMS-Mockup-FOOTER.html";
+?> 
